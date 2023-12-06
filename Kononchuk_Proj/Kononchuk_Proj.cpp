d00f75716fa6ab10248df0d2887beb994cdd5639
tafych <140445932+tafych@users.noreply.github.com>
2023-12-06 18:37:38 +0300

    2 лр
    с логгером, без глобальных переменных, немного исправлен ввод чисел

--- Kononchuk_Proj/Kononchuk_Proj.cpp
+++ Kononchuk_Proj/Kononchuk_Proj.cpp
@@ -5,38 +5,57 @@
 #include <fstream>
 #include<sstream>
 #include<algorithm>
+#include<cstdio>
+
 using namespace std;
+void slog() {}
+void logg(char const* func, char const* tosay, long line) {
+    cerr <<tosay<<" " << func << " at " << line << endl;
+    slog();
+}
+#define slog() logg(__func__,"Entered",__LINE__)
+#define elog() logg(__func__,"Exited",__LINE__)
 void intCheck(int& intt, int min, int max)
 {
+    slog();
     while (!(std::cin >> intt) or intt > max or intt < min) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "Invalid input, please re-enter.\n";
     }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    elog();
 }
 void posFloatCheck(float& floatt)
 {
+    slog();
     while (!(std::cin >> floatt) or floatt < 0) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "Invalid input, please re-enter.\n";
     }
+    elog();
 }
 void floatCheck(float& floatt)
 {
+    slog();
     while (!(std::cin >> floatt)) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "Invalid input, please re-enter.\n";
     }
+    elog();
 }
 void boolCheck(bool& booll)
 {
+    slog();
     while (!(std::cin >> booll)) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::cout << "Invalid input, please re-enter.\n";
     }
+    elog();
 }
 struct pipe
 {
@@ -55,17 +74,14 @@ struct KS
         return pow(WorkingMachines * Productivity, 0.66);
     }
 };
-vector<pipe> pipes;
-vector<KS> KSs;
-vector<int> HLP;
-vector<int> HLK;
-bool toDelP(pipe p){
-    return find(HLP.begin(), HLP.end(), stoi(p.Name)) != HLP.end();
-}
-bool toDelK(KS k) {
-    return find(HLK.begin(), HLK.end(), stoi(k.Name)) != HLK.end();
-}
-void highLightPinp() {
+//bool toDelP(vector<int>& HLP, pipe p) {
+//    return find(HLP.begin(), HLP.end(), stoi(p.Name)) != HLP.end();
+//}
+//bool toDelK(vector<int>& HLK, KS k) {
+//    return find(HLK.begin(), HLK.end(), stoi(k.Name)) != HLK.end();
+//}
+void highLightPinp(vector<pipe> pipes, vector<int>& HLP) {
+    slog();
     HLP.clear();
     cout << "Which ones?" << endl;
     string word;
@@ -96,7 +112,7 @@ void highLightPinp() {
         }
         else {
             try {
-                if (stoi(word)>pipes.size()-1) {
+                if (stoi(word) > pipes.size() - 1) {
                     cout << "Input incorrect." << endl;
                     HLP.clear();
                     break;
@@ -115,8 +131,10 @@ void highLightPinp() {
     sort(HLP.begin(), HLP.end());
     auto last = unique(HLP.begin(), HLP.end());
     HLP.erase(last, HLP.end());
+    elog();
 }
-void highLightKinp() {
+void highLightKinp(vector<KS> KSs, vector<int>& HLK) {
+    slog();
     HLK.clear();
     cout << "Which ones?" << endl;
     string word;
@@ -166,41 +184,90 @@ void highLightKinp() {
     sort(HLK.begin(), HLK.end());
     auto last = unique(HLK.begin(), HLK.end());
     HLK.erase(last, HLK.end());
+    elog();
 }
 void menu()
 {
-    cout << "1 - add pipe\n" << "2 - add KS\n" << "3 - show highlited\n" << "4 - highlight pipes\n" << "5 - highlite KSs\n" << "6 - edit highlited pipes\n" << "7 - edit highlited KSs\n" << "8 - delete highlited pipes\n" << "9 - delete highlited KSs\n" << "Input 0 or any non-integer to exit\n\nInput option:";
+    slog();
+    cout << "1 - add pipe\n"
+        << "2 - add KS\n"
+        << "3 - add unique pipe\n"
+        << "4 - add unique KS\n"
+        << "5 - show highlited\n"
+        << "6 - highlight pipes\n"
+        << "7 - highlite KSs\n" 
+        <<"8 - edit highlited pipes\n"
+        << "9 - edit highlited KSs\n"
+        << "10- delete highlited pipes\n" 
+        << "11 - delete highlited KSs\n" 
+        << "Input 0 to exit\n\nInput option:";
+    elog();
 }
 //1
 void addPipe(vector<pipe>& vec, string n, float l, float d, bool m)
 {
-    pipe newPipe{n,l,d,m};
+    slog();
+    pipe newPipe{ n,l,d,m };
     vec.push_back(newPipe);
+    elog();
 }
-void addPipes() {
+void addPipes(vector<pipe>& pipes) {
+    slog();
     cout << "How many: ";
     int amo;
     intCheck(amo, 0, 2147483647);
     for (int i = 0;i < amo;i++) {
         addPipe(pipes, to_string(pipes.size()), 10, 10, 0);
     }
+    elog();
+}
+void addSpecialPipe(vector<pipe>& pipes) {
+    slog();
+    float l, d;
+    bool m;
+    cout << "Length: " << endl;
+    posFloatCheck(l);
+    cout << "Diameter: " << endl;
+    posFloatCheck(d);
+    cout << "In maintenance: " << endl;
+    boolCheck(m);
+    addPipe(pipes, to_string(pipes.size()), l, d, m);
+    elog();
 }
 //2
 void addKS(vector<KS>& vec, string n, float am, float wm, float p)
 {
-    KS newKS{n,am,wm,p};
+    slog();
+    KS newKS{ n,am,wm,p };
     vec.push_back(newKS);
+    elog();
 }
-void addKSs() {
+void addKSs(vector<KS>& KSs) {
+    slog();
     cout << "How many: ";
     int amo;
     intCheck(amo, 0, 2147483647);
     for (int i = 0;i < amo;i++) {
         addKS(KSs, to_string(KSs.size()), 10, 10, 10);
     }
+    elog();
 }
+void addSpecialKS(vector<KS>& KSs) {
+    slog();
+    float am, wm,p;
+    cout << "All machines: " << endl;
+    posFloatCheck(am);
+    cout << "Working machines: " << endl;
+    posFloatCheck(wm);
+    cout << "Productivity: " << endl;
+    posFloatCheck(p);
+    addKS(KSs, to_string(KSs.size()), am, wm, p);
+    elog();
+}
+
 //3
-void oAll() {
+void oAll(vector<pipe>& pipes, vector<KS>& KSs, vector<int>& HLP, vector<int>& HLK) {
+    slog();
     cout << "Pipes:\n";
     for (int i = 0; i < HLP.size(); i++) {
         cout << "Name: " << pipes[HLP[i]].Name << "; Length: " << pipes[HLP[i]].Length << "; Diameter: " << pipes[HLP[i]].Diameter << "; Under Maintenace: " << pipes[HLP[i]].InMaintenance << endl;
@@ -209,9 +276,11 @@ void oAll() {
     for (int i = 0; i < HLK.size(); i++) {
         cout << "Name: " << KSs[HLK[i]].Name << "; All Machines: " << KSs[HLK[i]].AllMachines << "; Working Machines: " << KSs[HLK[i]].WorkingMachines << "; Machine Productivity: " << KSs[HLK[i]].Productivity << "; KS Efficiency:" << KSs[HLK[i]].Efficiency() << endl;
     };
+    elog();
 }
 //4
-void HLPipes() {
+void HLPipes(vector<pipe> pipes,vector<int>& HLP) {
+    slog();
     HLP.clear();
     int opti;
     cout << "1 - all pipes\n2 - choose by name\n3 - choose by maintenance\n";
@@ -226,7 +295,7 @@ void HLPipes() {
     break;
     case 2:
     {
-        highLightPinp();
+        highLightPinp(pipes,HLP);
     }
     break;
     case 3:
@@ -242,9 +311,11 @@ void HLPipes() {
     }
     break;
     }
+    elog();
 }
 //5
-void HLKSs() {
+void HLKSs(vector<KS> KSs,vector<int>& HLK) {
+    slog();
     HLK.clear();
     int opti;
     cout << "1 - all KSs\n2 - choose by name\n3 - choose by % of machines working\n";
@@ -259,7 +330,7 @@ void HLKSs() {
     break;
     case 2:
     {
-        highLightKinp();
+        highLightKinp(KSs,HLK);
     }
     break;
     case 3:
@@ -279,61 +350,76 @@ void HLKSs() {
     }
     break;
     }
+    elog();
 }
 //6
 
-void rePipe() {
+void rePipe(vector<pipe>& pipes, vector<int>& HLP) {
+    slog();
     bool m;
-    float l, d;
-    cout << "Length: ";
+    //float l, d;
+    /*cout << "Length: ";
     posFloatCheck(l);
     cout << "Diameter: ";
-    posFloatCheck(d);
+    posFloatCheck(d);*/
     cout << "Under Maintenance: ";
     boolCheck(m);
     for (int i = 0;i < HLP.size();i++) {
-        pipes[HLP[i]].Length = l;
-        pipes[HLP[i]].Diameter = d;
+        //pipes[HLP[i]].Length = l;
+        //pipes[HLP[i]].Diameter = d;
         pipes[HLP[i]].InMaintenance = m;
     };
+    elog();
 }
 //7
-void reKS() {
-    float am, wm, p;
-    cout << "All Machines: ";
-    posFloatCheck(am);
+void reKS(vector<KS>& KSs, vector<int>& HLK) {
+    slog();
+    //float am, wm, p;
+    float wm;
+    //cout << "All Machines: ";
+    //posFloatCheck(am);
     cout << "Working Machines: ";
     posFloatCheck(wm);
-    cout << "Machine Productivity: ";
-    floatCheck(p);
+    //cout << "Machine Productivity: ";
+    //floatCheck(p);
     for (int i = 0;i < HLK.size();i++) {
-        KSs[HLK[i]].AllMachines = am;
+        //KSs[HLK[i]].AllMachines = am;
         KSs[HLK[i]].WorkingMachines = wm;
-        KSs[HLK[i]].Productivity = p;
+        //KSs[HLK[i]].Productivity = p;
     };
+    elog();
 }
 //8
-void delHLP() {
+void delHLP(vector<pipe>& pipes, vector<int>& HLP) {
+    slog();
     vector<pipe>::iterator it;
-    it = remove_if(pipes.begin(), pipes.end(), toDelP);
+    it = remove_if(pipes.begin(), pipes.end(), [=](pipe p){
+        return find(HLP.begin(), HLP.end(), stoi(p.Name)) != HLP.end();
+        });
     pipes.erase(it, pipes.end());
     for (int i = 0;i < pipes.size();i++) {
         pipes[i].Name = to_string(i);
     }
     HLP.clear();
+    elog();
 }
 //9
-void delHLK() {
+void delHLK(vector<KS>& KSs, vector<int>& HLK) {
+    slog();
     vector<KS>::iterator it;
-    it = remove_if(KSs.begin(), KSs.end(), toDelK);
+    it = remove_if(KSs.begin(), KSs.end(), [=](KS k) {
+        return find(HLK.begin(), HLK.end(), stoi(k.Name)) != HLK.end();
+        });
     KSs.erase(it, KSs.end());
     for (int i = 0;i < KSs.size();i++) {
         KSs[i].Name = to_string(i);
     }
     HLK.clear();
+    elog();
 }
 //10
-void sAll() {
+void sAll(vector<pipe>& pipes, vector<KS>& KSs) {
+    slog();
     cout << "Input File Name (with .txt extension): ";
     string fname;
     cin >> fname;
@@ -347,9 +433,11 @@ void sAll() {
         saveFile << KSs[i].Name << ' ' << KSs[i].AllMachines << ' ' << KSs[i].WorkingMachines << ' ' << KSs[i].Productivity << ' ';
     }
     saveFile.close();
+    elog();
 }
 //11
-void lAll() {
+void lAll(vector<pipe>& pipes, vector<KS>& KSs) {
+    slog();
     string fname;
     pipes.clear();
     KSs.clear();
@@ -371,83 +459,108 @@ void lAll() {
         loadFile >> n >> f1 >> f2 >> f3;
         addKS(KSs, n, f1, f2, f3);
     }
+    elog();
 }
 int main()
-//8
-//9
 {
+    freopen("log.txt", "w", stderr);
+    vector<pipe> pipes;
+    vector<KS> KSs;
+    vector<int> HLP;
+    vector<int> HLK;
     int option = 15;
     while (option != 0) {
         cout << "-----~~~~===#(0)#===~~~~-----\n";
         menu();
-        cin >> option;
+        intCheck(option,0,11);
+
         switch (option) {
+        case 0:
+        {
+            cout << "Are you sure you want to exit? Not saved data will be lost" << endl << "Y - Yes, anything else - No"<<endl;
+            string ans = "";
+            cin >> ans;
+            if (ans != "Y") { option = 15; };
+        }
+        break;
             //1 - Добавить пустую трубу 
         case 1:
         {
-            addPipes();
+            addPipes(pipes);
         }
         break;
         //2 - Добавить пустую КС
         case 2:
         {
-            addKSs();
+            addKSs(KSs);
+        }
+        break;
+        //3 - Добавить непустую трубу
+        case 3:
+        {
+            addSpecialPipe(pipes);
+        }
+        break;
+        //4 - Добавить непустую КС
+        case 4:
+        {
+            addSpecialKS(KSs);
         }
         break;
         //3 - Вывести выделенные элементы
-        case 3:
+        case 5:
         {
-            oAll();
+            oAll(pipes,KSs,HLP,HLK);
         }
         break;
         //4 - Выделить нужные трубы
-        case 4:
+        case 6:
         {
-            HLPipes();
+            HLPipes(pipes, HLP);
         }
         break;
         //5 - Выделить нужные КС
-        case 5:
+        case 7:
         {
-            HLKSs();
+            HLKSs(KSs,HLK);
         }
         break;
         //6 - Редактировать выделенные трубы
-        case 6:
+        case 8:
         {
-            rePipe();
+            rePipe(pipes,HLP);
             ;}
         break;
         //7 - Редактировать выделенные КС
-        case 7:
+        case 9:
         {
-            reKS();
+            reKS(KSs,HLK);
             ;}
         break;
         //8 - удалить выделенные трубы
-        case 8:
+        case 10:
         {
-            delHLP();
+            delHLP(pipes,HLP);
         }
         break;
         //9 - удалить выделенные КС
-        case 9:
+        case 11:
         {
-            delHLK();
+            delHLK(KSs,HLK);
         }
         break;
         //10 - сохранить всё
-        case 10:
+        case 12:
         {
-            sAll();
+            sAll(pipes,KSs);
         }
-            break;
+        break;
         //11 - загрузить всё
-        case 11:
+        case 13:
         {
-            lAll();
+            lAll(pipes, KSs);
         }
-            break;
+        break;
         }
     }
 }
